b2baa12f4fe47bf159d67e7d68c6fb0859b63e6a
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-08 02:08:05 +0530

    Added IP request lockdown

--- status.py
+++ status.py
@@ -1,18 +1,19 @@
 import subprocess
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/status/<vmid>/<key>')
 def status(vmid, key):
     if key == "test":
-     vmid = int(vmid)
-     cmd = "/usr/bin/virsh list --all | grep kvm%s | awk {'print $3'}" % vmid
-     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-     (output, err) = p.communicate()
-     if output == "":
-      output = "missing"
-     return output
-
+        if request.remote_addr == "YOUR_IP_HERE":
+            vmid = int(vmid)
+            cmd = "/usr/bin/virsh list --all | grep kvm%s | awk {'print $3'}" % vmid
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+            (output, err) = p.communicate()
+            if output == "":
+                output = "missing"
+            return output
 
 if __name__ == '__main__':
     app.debug = True
