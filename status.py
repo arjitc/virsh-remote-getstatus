42c4559157cea2f6f6b97e7130db08642cbb9324
Arjit Chaudhary <arjit.c@gmail.com>
2015-07-07 22:29:15 +0530

    Create status.py

---
+++ status.py
@@ -0,0 +1,18 @@
+import subprocess
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/status/<vmid>/<key>')
+def status(vmid, key):
+    if key == "test":
+     cmd = "/usr/bin/virsh list --all | grep kvm%s | awk {'print $3'}" % vmid
+     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+     (output, err) = p.communicate()
+     if output == "":
+      output = "offline"
+     return output
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
